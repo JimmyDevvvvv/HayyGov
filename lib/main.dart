b3c11c628316d128c232336aa15f65c556e6ede7
WelloEssam <154703807+WelloEssam@users.noreply.github.com>
2025-04-18 02:21:53 +0200

    aaaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/home_screen.dart';
+import 'screens/announcement_feed_screen.dart';
+import 'screens/voting_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,11 +19,44 @@ class EmergencyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Emergency Numbers',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+      title: 'Emergency App',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const AppNavigator(),
+    );
+  }
+}
+
+class AppNavigator extends StatefulWidget {
+  const AppNavigator({super.key});
+
+  @override
+  State<AppNavigator> createState() => _AppNavigatorState();
+}
+
+class _AppNavigatorState extends State<AppNavigator> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    AnnouncementFeedScreen(),
+    VotingScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.phone), label: 'Emergency'),
+          BottomNavigationBarItem(icon: Icon(Icons.campaign), label: 'Announcements'),
+          BottomNavigationBarItem(icon: Icon(Icons.poll), label: 'Polls'),
+        ],
       ),
-      home: HomeScreen(),
     );
   }
 }
